abd8de543e3f7104920f378686a2b3694b5521fa
Rizel Scarlett <blackgirlbytes@github.com>
2023-03-11 20:04:06 +0000

    sendTweet and sendToot

---
+++ sendToot.py
@@ -0,0 +1,19 @@
+# send a toot to Mastodon with Mastodon.py
+import os
+# import Mastodon.py
+from mastodon import Mastodon
+
+
+# Consumer keys and access tokens, use for Mastodon.Client authentication which are stored in Codespaces secrets prefix with MASTODON_
+mastodon = Mastodon(
+    client_id=os.environ['MASTODON_CLIENT_KEY'],
+    client_secret=os.environ['MASTODON_CLIENT_SECRET'],
+    access_token=os.environ['MASTODON_ACCESS_TOKEN'],
+    api_base_url="https://hachyderm.io",
+)
+
+# create variable called toot with the text that says "I wrote this toot with Copilot!"
+toot = "test test"
+
+# Send toot to Mastodon
+mastodon.toot(toot)
